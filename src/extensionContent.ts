2e4b1658750933d34a8213eb2d1ab52f8daab405
Sébastien CAPARROS <sebastien@hivelocity.co.jp>
2019-03-13 14:06:58 +0900

    Filtering nodes to transform

--- src/extensionContent.ts
+++ src/extensionContent.ts
@@ -1,7 +1,30 @@
+import CharType from './Misc/CharType';
+
+const containsJapanese = (text: string) => {
+	for (let i = 0; i < text.length; i++) {
+		if (CharType.isJapanese(text[i])) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
 const walker = document.createTreeWalker(
 	document.body,
 	NodeFilter.SHOW_TEXT,
 );
 while (walker.nextNode()) {
-	console.log('myext', walker.currentNode.data);
+	const textNode: Text = <Text>walker.currentNode;
+	const text = textNode.data.trim();
+	const containerType: string = (<Element>textNode.parentNode).tagName.toLowerCase();
+	if (
+		text.length > 0
+		&& containerType !== 'script'
+		&& containerType !== 'style'
+		&& containerType !== 'noscript'
+		&& containsJapanese(text)
+	) {
+		console.log('myext', textNode);
+	}
 }

af1b7a2620a5319b1f69a8047e5e4e1fcacd316a
Sébastien Caparros <sebastien@caparros.me>
2019-03-25 22:53:25 +0900

    Working on displaying the tokens

---
+++ src/Api/tokenize.ts
@@ -0,0 +1,16 @@
+import Token from '../Lexer/Token/Token';
+import Serializer from '../Api/Serializer';
+
+const serializer = new Serializer();
+
+export default async (text: string): Promise<Token[]> => {
+	const response = await fetch('http://localhost:3000/tokenize', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ sentence: text }),
+	});
+	const responseData = await response.json();
+	return serializer.fromJsonApi<Token[]>(responseData);
+};
--- src/extensionContent.ts
+++ src/extensionContent.ts
@@ -1,6 +1,8 @@
 import CharType from './Misc/CharType';
 import Serializer from './Api/Serializer';
 import Token from './Lexer/Token/Token';
+import tokenize from './Api/tokenize';
+const elementVisible = require('element-visible');
 
 const containsJapanese = (text: string) => {
 	for (let i = 0; i < text.length; i++) {
@@ -12,14 +14,79 @@ const containsJapanese = (text: string) => {
 	return false;
 };
 
-const serializer = new Serializer();
+const TAG_SENTENCE = 'yometai-sentence';
+const TAG_TOKEN = 'yometai-token';
+const style = document.createElement('style');
+style.textContent = `
+	${TAG_SENTENCE} {
+        clear: both;
+        margin-bottom: 30px;
+        display: block;
+    }
+	${TAG_SENTENCE} > ${TAG_TOKEN} {
+        float: left;
+        margin-bottom: 15px;
+        height: 2rem;
+        text-align: center;
+    }
 
-(async () => {
+    ${TAG_SENTENCE} > ${TAG_TOKEN}:nth-child(odd) {
+        background: #DDDDDD;
+    }
+
+	${TAG_SENTENCE} > ${TAG_TOKEN} .furigana,
+	${TAG_SENTENCE} > ${TAG_TOKEN} .meaning {
+        font-size: 0.5rem;
+        display: block;
+    }
+`;
+document.body.appendChild(style);
+
+customElements.define(TAG_SENTENCE, class extends HTMLElement {});
+customElements.define(TAG_TOKEN, class extends HTMLElement {});
+
+const convertNode = (node: Text, tokens: Token[]) => {
+	const container = document.createElement(TAG_SENTENCE);
+
+	tokens.map((token) => {
+		const span = document.createElement(TAG_TOKEN);
+
+		const furigana = 'xx';
+		const meaning = 'xx';
+
+		span.innerHTML = `
+			<span class="furigana">${furigana}</span>
+			${token.text}
+			<span class="meaning">${meaning}</span>
+		`;
+
+		container.appendChild(span);
+	});
+
+	(<Node>node.parentNode).replaceChild(container, node);
+};
+
+const hasParentTag = (node: Node, tag: string): boolean => {
+	let cur: Node = node;
+
+	do {
+		if ((<Element>cur).tagName.toLowerCase() === tag) {
+			return true;
+		}
+
+		cur = <Node>cur.parentNode;
+	} while (cur !== null);
+
+	return false;
+};
+
+const getElementsToConvert = (): Text[] => {
 	const walker = document.createTreeWalker(
 		document.body,
 		NodeFilter.SHOW_TEXT,
 	);
 
+	const elements: Text[] = [];
 	while (walker.nextNode()) {
 		const textNode: Text = <Text>walker.currentNode;
 		const text = textNode.data.trim();
@@ -30,17 +97,31 @@ const serializer = new Serializer();
 			&& containerType !== 'style'
 			&& containerType !== 'noscript'
 			&& containsJapanese(text)
+			&& elementVisible(textNode.parentNode)
+			&& !hasParentTag(textNode, TAG_SENTENCE)
 		) {
-			const response = await fetch('http://localhost:3000/tokenize', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ sentence: text }),
-			});
-			const responseData = await response.json();
-			const data = serializer.fromJsonApi<Token[]>(responseData);
-			console.log(data); // TODO
+			elements.push(textNode);
 		}
 	}
-})().catch(console.error);
+
+	return elements;
+};
+
+const convertVisibleElements = async () => {
+	const elements = getElementsToConvert();
+	for (let i = 0; i < elements.length; i++) {
+		try {
+			const textNode = elements[i];
+			const text = textNode.data.trim();
+			const data = await tokenize(text);
+			convertNode(textNode, data);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+};
+
+convertVisibleElements(); // Initializing
+// window.addEventListener('scroll', () => {
+// 	convertVisibleElements();
+// });

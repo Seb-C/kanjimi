903fea2221443c9b9ba0d663336d1069bb3384e0
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-03-24 23:22:42 +0900

    Testing the conjugation model

---
+++ tests/Common/Models/Conjugation.ts
@@ -0,0 +1,18 @@
+import 'jasmine';
+import Conjugation from 'Common/Models/Conjugation';
+import ConjugationType from 'Common/Types/ConjugationType';
+
+describe('Conjugation', () => {
+	it('API formatting methods', async () => {
+		const input = new Conjugation(
+			'conjugation',
+			'dictionary form',
+			ConjugationType.IMPERATIVE,
+		);
+		const output = Conjugation.fromApi(input.toApi());
+
+		expect(output.conjugation).toBe('conjugation');
+		expect(output.dictionaryForm).toBe('dictionary form');
+		expect(output.type).toBe(ConjugationType.IMPERATIVE);
+	});
+});

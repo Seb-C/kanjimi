83f772e5340c6ee1dc003ef096af27b7e0883c77
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-03-24 23:44:38 +0900

    Testing the Token model

---
+++ tests/Common/Models/Token.ts
@@ -0,0 +1,47 @@
+import 'jasmine';
+import Conjugation from 'Common/Models/Conjugation';
+import Token from 'Common/Models/Token';
+import ConjugationType from 'Common/Types/ConjugationType';
+import Word from 'Common/Models/Word';
+import TokenType from 'Common/Types/TokenType';
+import Language from 'Common/Types/Language';
+
+describe('Token', () => {
+	it('API formatting methods', async () => {
+		const input = new Token(
+			'text',
+			TokenType.PARTICLE,
+			[
+				new Word('', '', Language.FRENCH, '', []),
+				new Word('', '', Language.SPANISH, '', []),
+			],
+			'verb',
+			'conjugation',
+			[
+				new Conjugation('', '', ConjugationType.PAST),
+				new Conjugation('', '', ConjugationType.CONDITIONAL),
+			],
+		);
+		const output = Token.fromApi(JSON.parse(JSON.stringify(input.toApi())));
+
+		expect(output.text).toBe('text');
+		expect(output.type).toBe(TokenType.PARTICLE);
+		expect(output.words.length).toBe(2);
+		expect(output.words[0].translationLang).toBe(Language.FRENCH);
+		expect(output.words[1].translationLang).toBe(Language.SPANISH);
+		expect(output.verb).toBe('verb');
+		expect(output.conjugation).toBe('conjugation');
+		expect(output.forms.length).toBe(2);
+		expect(output.forms[0].type).toBe(ConjugationType.PAST);
+		expect(output.forms[1].type).toBe(ConjugationType.CONDITIONAL);
+
+		// Now testing the null values
+		const input2 = new Token('', TokenType.PARTICLE, [], null, null, []);
+		const output2 = Token.fromApi(JSON.parse(JSON.stringify(input2.toApi())));
+
+		expect(output2.words.length).toBe(0);
+		expect(output2.verb).toBe(null);
+		expect(output2.conjugation).toBe(null);
+		expect(output2.forms.length).toBe(0);
+	});
+});

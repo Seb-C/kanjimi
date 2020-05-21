ac85a1788a762929f70da9c3c5c53c17a138928e
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-05-21 20:22:29 +0900

    Dumping the Cypress launch options to debug the CI

--- cypress.plugins.js
+++ cypress.plugins.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = (on, config) => {
 	on('before:browser:launch', (browser = {}, launchOptions) => {
 		launchOptions.extensions.push(path.resolve(__dirname, './extension'));
+		console.log('Launch options:', launchOptions);
 
 		return launchOptions;
 	});

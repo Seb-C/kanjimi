4adad7637a91227047e8c3ad0d1d790d0e9d25a7
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-06-14 23:03:13 +0900

    Displaying the Kanji SVGs

--- src/Extension/Components/Kanjis.vue
+++ src/Extension/Components/Kanjis.vue
@@ -1,9 +1,10 @@
 <template>
-	<div class="kanjis">
-		<div
-			v-for="kanji of kanjis"
-		>
-			<img v-bind:src="kanji" />
+	<div v-if="images === null" class="kanjimi-loader">
+		Loading...
+	</div>
+	<div v-else class="kanjis">
+		<div v-for="image of images">
+			<svg v-html="image" />
 		</div>
 	</div>
 </template>
@@ -16,16 +17,33 @@
 		props: {
 			token: { type: Token },
 		},
-		computed: {
-			kanjis(): string[] {
-				return this.token.text.split('').filter(
+		data() {
+			return {
+				kanjis: this.token.text.split('').filter(
 					(char: string) => CharType.of(char) === CharType.KANJI
-				).map(kanji => (
-					`${process.env.KANJIMI_WWW_URL}/img/KanjiVG/0${kanji.charCodeAt(0).toString(16)}.svg`
-				));
-			},
+				),
+				images: <string[]|null>null,
+			};
+		},
+		async created() {
+			this.images = await Promise.all(
+				this.kanjis.map(async (kanji: string) => {
+					const url = `${process.env.KANJIMI_WWW_URL}/img/KanjiVG/0${kanji.charCodeAt(0).toString(16)}.svg`;
+					const response = await fetch(url);
+					const svg = await response.text();
+					return svg;
+				}),
+			);
 		},
 	});
 </script>
 <style scoped>
+	.kanjis >>> svg,
+	.kanjis >>> svg * {
+		all: unset;
+	}
+
+	.kanjis >>> svg text {
+		font-size: 0.5em;
+	}
 </style>

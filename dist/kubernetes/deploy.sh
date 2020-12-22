e5d7b238b5132c4c578f1f62b01a95d1e67e6474
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2020-12-22 23:54:29 +0900

    Fixing the kubernetes configuration

--- dist/kubernetes/deploy.sh
+++ dist/kubernetes/deploy.sh
@@ -2,29 +2,26 @@
 
 set -e
 
-doctl registry login
-
-docker build \
-    -t registry.digitalocean.com/kanjimi/kanjimi:server \
-    --build-arg NODE_ENV=production \
-    --build-arg KANJIMI_API_URL=https://www.kanjimi.com/api \
-    --build-arg KANJIMI_WWW_URL=https://www.kanjimi.com \
-    -f ./dist/docker/server.Dockerfile \
-    .
-docker build \
-    -t registry.digitalocean.com/kanjimi/kanjimi:nginx \
-    --build-arg KANJIMI_NGINX_REMOVE_TEST_PAGES=true \
-    --build-arg KANJIMI_NGINX_CERTIFICATE_DOMAIN=www.kanjimi.com \
-    --build-arg KANJIMI_API_URL=https://www.kanjimi.com/api \
-    --build-arg KANJIMI_WWW_URL=https://www.kanjimi.com \
-    -f ./dist/docker/nginx.Dockerfile \
-    .
-
-docker push registry.digitalocean.com/kanjimi/kanjimi:server
-docker push registry.digitalocean.com/kanjimi/kanjimi:nginx
+#docker build \
+#    -t registry.digitalocean.com/kanjimi/kanjimi:server \
+#    --build-arg NODE_ENV=production \
+#    --build-arg KANJIMI_API_URL=https://www.kanjimi.com/api \
+#    --build-arg KANJIMI_WWW_URL=https://www.kanjimi.com \
+#    -f ./dist/docker/server.Dockerfile \
+#    .
+#docker build \
+#    -t registry.digitalocean.com/kanjimi/kanjimi:nginx \
+#    --build-arg KANJIMI_NGINX_REMOVE_TEST_PAGES=true \
+#    --build-arg KANJIMI_NGINX_CERTIFICATE_DOMAIN=www.kanjimi.com \
+#    --build-arg KANJIMI_API_URL=https://www.kanjimi.com/api \
+#    --build-arg KANJIMI_WWW_URL=https://www.kanjimi.com \
+#    -f ./dist/docker/nginx.Dockerfile \
+#    .
+#
+#docker push registry.digitalocean.com/kanjimi/kanjimi:server
+#docker push registry.digitalocean.com/kanjimi/kanjimi:nginx
 
 kubectl apply \
-    --filename ./dist/kubernetes/namespace.yaml \
     --filename ./dist/kubernetes/config.yaml \
     --filename ./dist/kubernetes/logs-volume.yaml \
     --filename ./dist/kubernetes/config-fluentbit-nginx.yaml \
@@ -32,9 +29,10 @@ kubectl apply \
     --filename ./dist/kubernetes/fluentbit-storage-deployment.yaml \
     --filename ./dist/kubernetes/fluentbit-storage-service.yaml \
     --filename ./dist/kubernetes/server-deployment.yaml \
-    --filename ./dist/kubernetes/server-service.yaml \
     --filename ./dist/kubernetes/server-hpa.yaml \
+    --filename ./dist/kubernetes/server-load-balancer.yaml \
+    --namespace default \
     --prune \
     --all
 
-kubectl rollout restart deployment server-deployment --namespace=kanjimi
+#kubectl rollout restart deployment server-deployment --namespace=default
